5caf1857e82632040e9e4c544b0bf21d95ac2f91
Brett Chien <brett.chien@gmail.com>
2014-09-18 17:39:50 -0700

    update package description and version number

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@
 from setuptools import setup, find_packages
 
 setup(name="PyBLE",
-      version="0.2",
-      description="Python Bluetooth Smart(BLE) manager wrapper for OSX, Linux and Windows 8",
+      version="0.2.1",
+#      description="Python Bluetooth Smart(BLE) manager wrapper for OSX, Linux and Windows 8",
+      description="Python Bluetooth Smart(BLE) manager wrapper for OSX",
       author="Brett Chien",
       author_email="brett.chien@gmail.com",
       url="",

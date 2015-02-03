4b6739c935f5fdba8d9b647b4164a220df3b9fac
Brett Chien <brett.chien@gmail.com>
2015-02-03 13:11:46 -0800

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="PyBLE",
-      version="0.2.1",
+      version="0.2.2",
 #      description="Python Bluetooth Smart(BLE) manager wrapper for OSX, Linux and Windows 8",
       description="Python Bluetooth Smart(BLE) manager wrapper for OSX",
       author="Brett Chien",

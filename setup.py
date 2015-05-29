ebf09a5cd0a9b91c29759da6f16a5a029744ac0d
Brett Chien <brett.chien@gmail.com>
2015-05-29 18:23:40 +0800

    modified setup.py, bump version

--- setup.py
+++ setup.py
@@ -13,18 +13,21 @@ else:
     # windows
     pass
 
-versionStr = "0.2.5"
+versionStr = "0.2.6"
 
-setup(name="PyBLEWrapper",
-      version=versionStr,
-#      description="Python Bluetooth Smart(BLE) manager wrapper for OSX, Linux and Windows 8",
-      description="Python Bluetooth Smart(BLE) manager wrapper for OSX",
-      author="Brett Chien",
-      author_email="brett.chien@gmail.com",
-      license="MIT",
-      url="https://github.com/brettchien/PyBLEWrapper",
-      download_url='https://github.com/brettchien/PyBLEWrapper/tarball/' + versionStr,
-      packages=find_packages(exclude="tests"),
-      install_requires=requirements,
-      keywords = ['Blueooth', 'BLE', 'Bluetooth Low Energy', 'osx']
-      )
+setup(
+        name="PyBLEWrapper",
+        version=versionStr,
+        #      description="Python Bluetooth Smart(BLE) manager wrapper for OSX, Linux and Windows 8",
+        description="Python Bluetooth Smart(BLE) manager wrapper for OSX",
+        author="Brett Chien",
+        author_email="brett.chien@gmail.com",
+        license="MIT",
+        url="https://github.com/brettchien/PyBLEWrapper",
+        download_url='https://github.com/brettchien/PyBLEWrapper/tarball/' + versionStr,
+        packages=find_packages(exclude="tests"),
+        install_requires=requirements,
+        keywords = ['Bluetooth Low Energy', 'BLE', 'OSX'],
+        classifiers = [],
+        )
+
